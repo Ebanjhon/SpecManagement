b55441de907274d29dcd3493dc7e801d5288add0
BEOBUJ <trackmix115@gmail.com>
2024-06-15 11:06:21 +0700

    Add API Room Chat

---
+++ QuanLyDeCuong/src/main/java/com/eban/DTO/ChatUserDTO.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eban.DTO;
+
+/**
+ *
+ * @author nmau4
+ */
+public class ChatUserDTO {
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String role;
+    private String avatar;
+    private Integer roomId;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * @return the avatar
+     */
+    public String getAvatar() {
+        return avatar;
+    }
+
+    /**
+     * @param avatar the avatar to set
+     */
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    /**
+     * @return the roomId
+     */
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    /**
+     * @param roomId the roomId to set
+     */
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+    
+}
---
+++ QuanLyDeCuong/src/main/java/com/eban/repositories/ChatRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eban.repositories;
+
+import com.eban.DTO.ChatUserDTO;
+import com.eban.DTO.RoomChatDTO;
+import java.util.List;
+
+/**
+ *
+ * @author nmau4
+ */
+public interface ChatRepository {
+
+    List<RoomChatDTO> findAllChatRoomsWithUsers(Integer userId);
+
+}
